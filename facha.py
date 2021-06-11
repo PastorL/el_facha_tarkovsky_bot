dc5074e45c3688f513858e27c79e9a88678d0256
Leonardo Luna <leonardo@psiware.com.ar>
2021-06-11 00:37:50 -0300

    Agrego validaciones de servidor y usuario maestro, logica de ejecucion sql.

--- facha.py
+++ facha.py
@@ -12,20 +12,25 @@ PG_US = open("PG_US.txt", 'r').read()
 PG_HS = open("PG_HS.txt", 'r').read()
 db = psycopg2.connect(host = PG_HS, database = PG_DB, user = PG_US, password = PG_PW)
 frases_respuestas = []
+servers_availables = []
+ignored_users = []
 
 
 @bot.event
 async def on_message(message):
-    ctx = await bot.get_context(message)
-    if message.author == bot.user:
-        return
-    if message.author.name == 'PsiwareBot':
-        psiware_bot_frase = message.content
-        await insert_frase(ctx, psiware_bot_frase, 'PsiwareBot')
-    if message.content in frases_respuestas:
-        await frase(ctx)
-    await bot.process_commands(message)
-
+    if message.author.name not in ignored_users:
+        ctx = await bot.get_context(message)
+        if validate_server(ctx) or await validate_pastor(ctx):
+            if message.author == bot.user:
+                return
+            if message.author.name == 'PsiwareBot':
+                psiware_bot_frase = message.content
+                await insert_frase(ctx, psiware_bot_frase, 'PsiwareBot')
+            if message.content in frases_respuestas:
+                await frase(ctx)
+            await bot.process_commands(message)
+        else:
+            await ctx.send('No te haga el loco y anda a escribir al servidor.')
 
 
 @bot.event
@@ -36,8 +41,16 @@ async def on_ready():
         frases = cursor.fetchall()
         for frase in frases:
             frases_respuestas.append(frase[0])
+        cursor.execute("SELECT description FROM available_servers")
+        servers = cursor.fetchall()
+        for server in servers:
+            servers_availables.append(server[0])
+        cursor.execute("SELECT user_name FROM ignored_users")
+        ignored_users_db = cursor.fetchall()
+        for user in ignored_users_db:
+            ignored_users.append(user[0])
     except Exception as exc:
-        print(f"No pude obtener las frases a responder: {exc}")
+        print(f"No pude obtener las frases  y servidores a responder: {exc}")
     await bot.change_presence(activity=discord.Game('esculpir el tiempo'))
     print('Привет друзья!')
 
@@ -60,8 +73,9 @@ async def deleteFrase(ctx,*,frase):
 @bot.command()
 async def frase(ctx):
     cursor = db.cursor()
+    server_name = ctx.message.guild.name
     try:
-        cursor.execute("SELECT descripcion FROM frases ORDER BY RANDOM() LIMIT 1")
+        cursor.execute(f"SELECT descripcion FROM frases WHERE server_name='{server_name}' ORDER BY RANDOM() LIMIT 1")
         frase = cursor.fetchone()
         await ctx.send(frase[0])
     except Exception as exc:
@@ -73,7 +87,8 @@ async def frase(ctx):
 async def insert_frase(ctx, frase, autor):
     if validate_frase(frase):
         cursor = db.cursor()
-        sql = (f"INSERT INTO frases(descripcion, usuario) VALUES ('{frase}','{autor}')")
+        server_name = ctx.message.guild.name
+        sql = (f"INSERT INTO frases(descripcion, usuario, server_name) VALUES ('{frase}','{autor}','{server_name}')")
         try:
             result = cursor.execute(sql)
             await ctx.send("Frase agregada товарищ!")
@@ -89,7 +104,8 @@ async def insert_frase(ctx, frase, autor):
 async def delete_frase(ctx, frase, autor):
     if not validate_frase(frase):
         cursor = db.cursor()
-        sql = (f"DELETE FROM frases WHERE descripcion = '{frase}'")
+        server_name = ctx.message.guild.name
+        sql = (f"DELETE FROM frases WHERE descripcion = '{frase}' AND server_name = '{server_name}'")
         try:
             result = cursor.execute(sql)
             await ctx.send("Frase borrada товарищ!")
@@ -129,6 +145,7 @@ async def buscame(ctx,*,movie_to_search):
     for movie in movies:
         if cont <= 5:
             try:
+                print(movie)
                 title = movie['title']
                 year = movie['year']
                 await ctx.send(f'{title} - {year}')
@@ -160,6 +177,7 @@ async def agregarTop(ctx,*,link_top):
             await ctx.send('No anda nada cuando guardo el top: {}'.format(exc))
     db.commit()
     cursor.close()
+
 
 
 def buscar_top(autor):
@@ -200,18 +218,64 @@ async def top(ctx,*,usuario):
 
 
 @bot.command()
+async def quienFueElHijoDePuta(ctx,*,frase):
+    if await validate_pastor(ctx):
+        cursor = db.cursor()
+        try:
+            cursor.execute(f"SELECT usuario FROM frases WHERE descripcion = '{frase}'")
+            culpable = cursor.fetchone()
+            if culpable == None:
+                await ctx.send("Disculpe maestro pero no encontre esa frase.")
+            else:
+                await ctx.send(f"El hije de pute fue: {culpable[0]}")
+        except Exception as exc:
+            await ctx.send(f"Error al traer al hijo de puta: {exc}")
+        finally:
+            cursor.close()
+
+
+
+def validate_server(ctx):
+    server_name = None
+    try:
+        server_name = ctx.message.guild.name
+    except Exception as exc:
+        print(f"{ctx.message.author.name} me esta hablando por privado")
+    if (server_name == None) or (server_name not in servers_availables):
+        return False
+    else:
+        return True
+
+
+
+@bot.command()
+async def ejecutar(ctx,*,sql):
+    if await validate_pastor(ctx):
+        cursor = db.cursor()
+        try:
+            cursor.execute(sql)
+            db.commit()
+            await ctx.send("Ejecutado master.")
+        except Exception as exc:
+            await ctx.send(f"No pude ejecutar el sql: {exc}")
+        finally:
+            cursor.close()
+
+
+
+async def validate_pastor(ctx):
+    autor = ctx.message.author.id
+    if autor == 186664188750462977:
+        return True
+    else:
+        await ctx.send("Quien te conoce pa? so boludo y no tene huevo.")
+        return False
+
+
+
+@bot.command()
 async def tasBien(ctx):
     await ctx.send(f'{round(bot.latency * 1000)}ms')
-
-
-@bot.command()
-async def stalker(ctx):
-    await ctx.send("https://www.youtube.com/watch?v=TGRDYpCmMcM")
-
-
-@bot.command()
-async def infoChess(ctx):
-    await ctx.send("https://drive.google.com/file/d/1CbPjXkNlE5d7gGXeoumgL-vfrG8T5Jkc/view")
 
 @bot.command()
 @commands.has_permissions(manage_messages=True)
