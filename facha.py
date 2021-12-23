815b93915e1e8aa18c301cc377471c2b49d64519
Leonardo Luna <leonardo@psiware.com.ar>
2021-12-23 18:06:35 -0300

    Agrego funcionalidad para agregar fechas de cumpleaños y para que devuelva el cumpleaños mas cercano o el del dia.

--- facha.py
+++ facha.py
@@ -487,29 +487,6 @@ async def validate_koke(ctx, koke):
 
 
 @bot.command()
-async def holasanti(ctx):
-    loc = ("xxx.xls")
-    wb = xlrd.open_workbook(loc)
-    sheet = wb.sheet_by_index(0)
-    rows = sheet.nrows
-    for i in range(rows):
-        movie_name = sheet.cell_value(i, 5)
-        imdb_link = sheet.cell_value(i, 6)
-        imdb_id = sheet.cell_value(i, 1)
-        cursor = db.cursor()
-        sql = (f"INSERT INTO  stalkers_movies(movie_name, imdb_link, imdb_id) VALUES ('{get_frase(movie_name)}','{get_frase(imdb_link)}','{get_frase(imdb_id)}')")
-        try:
-            result = cursor.execute(sql)
-            print(f"SE INSERTO EL KOKEMON: {movie_name}")
-        except Exception as exc:
-            print("EXPLOTO UN KOKEMON")
-            print(exc)
-        db.commit()
-        cursor.close()
-
-
-
-@bot.command()
 async def leon(ctx):
     today = date.today()
     remaining_days_to_agilucho = 0
@@ -596,6 +573,96 @@ def validate_parmi_frase(frase):
         print('Error al validar la frase.')
     finally:
         cursor.close()
+
+
+
+@bot.command()
+async def cumple(ctx):
+    discord_server = ctx.message.guild.name
+    cursor = db.cursor()
+    try:
+        cursor.execute(f"SELECT discord_user, discord_id, fecha, cast(fecha + ((extract(year from age(fecha)) + 1) * interval '1' year) as date) as next_birthday from cumpleaños_fechas WHERE to_char(CURRENT_DATE,'MM') = to_char(cast(fecha + ((extract(year from age(fecha)) + 1) * interval '1' year) as date), 'MM') AND to_char(CURRENT_DATE,'DD') = to_char(cast(fecha + ((extract(year from age(fecha)) + 1) * interval '1' year) as date), 'DD')")
+        cumpleañero_del_dia = cursor.fetchone()
+        if cumpleañero_del_dia != None:
+            cumpleañero_tag = f"<@{cumpleañero_del_dia[1]}>"
+            await ctx.send(f'Hoy es el cumpleaños de {cumpleañero_tag}! С днем ​​рождения товарищ!')
+        else:
+            cursor.execute(f"SELECT discord_user, discord_id, fecha, cast(fecha + ((extract(year from age(fecha)) + 1) * interval '1' year) as date) as next_birthday from cumpleaños_fechas WHERE discord_server = '{discord_server}' ORDER BY next_birthday asc LIMIT 1")
+            cumpleaños_data = cursor.fetchone()
+            cumpleañero = cumpleaños_data[0]
+            cumpleañero_id = cumpleaños_data[1]
+            cumple = cumpleaños_data[3]
+            await ctx.send(f'El próximo cumpleaños es el de {cumpleañero} el {cumple.day} del {cumple.month}.')
+    except:
+        await ctx.send(f'Error gravísimo al consultar los cumpleaños.')
+
+
+
+
+@bot.command()
+async def addMiCumple(ctx,*,fecha):
+    autor_name = ctx.message.author.name
+    server_name = ctx.message.guild.name
+    autor_id = autor = ctx.message.author.id
+    today = date.today()
+    fecha_array = []
+    if '/' in fecha or '-' in fecha:
+        if '/' in fecha:
+            fecha_array = fecha.split('/')
+        if '-' in fecha:
+            fecha_array = fecha.split('-')
+    else:
+        await ctx.send(f'Cuchame una cosa no te haga el vive y poné una fecha válida día/mes/año en ese exacto formato o te liquido.')
+    if (len(fecha_array) == 3 and len(fecha_array[0]) != 0 and len(fecha_array[1]) != 0 and len(fecha_array[2]) != 0):
+        dias = int(fecha_array[0])
+        mes = int(fecha_array[1])
+        año = int(fecha_array[2])
+        if (dias > 31 or dias < 1):
+            await ctx.send(f'No te haga el vive y poné un día válido.')
+        else:
+            if (mes > 12 or mes < 1):
+                await ctx.send(f'No te haga el vive y poné un mes válido.')
+            else:
+                if (año > today.year or año < 1800):
+                    await ctx.send(f'No te haga el vive y poné un año válido.')
+                else:
+                    if validate_cumpleanios_exists(server_name, autor_id):
+                        cursor = db.cursor()
+                        date_cumpleaños = date(año, mes, dias)
+                        sql = (f"INSERT INTO cumpleaños_fechas(discord_user, fecha, discord_server, discord_id) VALUES ('{autor_name}','{date_cumpleaños}','{server_name}','{autor_id}')")
+                        try:
+                            result = cursor.execute(sql)
+                            await ctx.send("Cumpleaños agregado товарищ!")
+                        except Exception as exc:
+                            await ctx.send('No anda nada cuando guardo el cumpleaños: {}'.format(exc))
+                        db.commit()
+                        cursor.close
+                    else:
+                        await ctx.send(f'Vos ya cargaste tu cumpleaños payase vola de acá o te reviento la gorra.')
+    else:
+        await ctx.send(f'Cuchame una cosa no te haga el vive y poné una fecha válida día/mes/año en ese exacto formato o te liquido.')
+
+
+def validate_cumpleanios_exists(server_name, autor_id):
+    cursor = db.cursor()
+    try:
+        cursor.execute(f"SELECT COUNT (*) FROM cumpleaños_fechas WHERE discord_server = '{server_name}' AND discord_id = '{autor_id}'")
+        result = 0
+        result = cursor.fetchone()[0]
+        if result == 0:
+            return True
+        else:
+            return False
+    except Exception as exc:
+        print('Error al validar el cumpleaños.')
+    finally:
+        cursor.close()
+
+
+
+@bot.command()
+async def holasanti(ctx):
+    print(ctx.message.author.name)
 
 
 
