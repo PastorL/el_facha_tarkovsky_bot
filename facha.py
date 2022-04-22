14c2bae4e4a0721d06c94af6235d6a5b639a3dfe
Leonardo Luna <leonardo@psiware.com.ar>
2022-04-22 17:07:15 -0300

    Arreglo el buscar frase

--- facha.py
+++ facha.py
@@ -124,7 +124,7 @@ async def buscarFrase(ctx, frase):
     cursor = db.cursor()
     server_name = ctx.message.guild.name
     try:
-        cursor.execute(f"SELECT descripcion FROM frases WHERE server_name='{server_name}' AND descripcion LIKE '{frase}' ORDER BY RANDOM() LIMIT 1")
+        cursor.execute(f"SELECT descripcion FROM frases WHERE server_name='{server_name}' AND descripcion LIKE '%{frase}%' ORDER BY RANDOM() LIMIT 1")
         frase = cursor.fetchone()
         await ctx.send(frase[0])
     except Exception as exc:
